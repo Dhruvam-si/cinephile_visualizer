7f86ee2f99b05ee43486b5eb3ea9d2a275f049a2
dhruvam Si <redfir347@gmail.com>
2026-06-15 14:08:37 +0530

    naming fixes and added message flashings

--- app.py
+++ app.py
@@ -3,53 +3,76 @@ from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = "diewreview"
-app.permanent_session_lifetime=timedelta(minutes=1)
-@app.route("/")
+#time for session storing
+app.permanent_session_lifetime=timedelta(minutes=5)
+
+#homepage/login page
+@app.route("/", methods=["GET", "POST"])
 def home():
+
+    if request.method == "POST":
+        session["firstname"] = request.form["faname"]
+        session["lastname"] = request.form["laname"]
+        session.permanent = True
+
+        return redirect(url_for("review"))
+
+    # GET request
+    if "firstname" in session and "lastname" in session:
+        flash("You are already logged in !","info")
+        return render_template(
+            "newind.html",
+            faname=session["firstname"],
+            laname=session["lastname"]
+        )
+
     return render_template("newind.html")
 
-@app.route("/review",methods=["GET","POST"])
-def review():
-    if request.method=="POST":
-        user = request.form["firstname"]
-        movie = request.form["movie"]
-        ver = request.form["verdict"]
-        session["lname"]=request.form["lastname"]
-        session.permanent=True
 
-        print(request.form)
-        print("User:", user)
-        print("Movie:", movie)
-        print("rev: ",ver)
-        print(url_for("user", usr=user, mov=movie,rev = ver))
-        
-        return redirect(url_for("user",usr=user,mov=movie,rev=ver) )
-    else:
-        return render_template("reviews.html")
+
+@app.route("/review", methods=["GET", "POST"])
+def review():
+
+    if request.method == "GET":
+        return render_template(
+            "write_review.html",
+            firstname=session["firstname"],
+            lastname=session["lastname"]
+        )
+
+    # POST
+    user = request.form["firstname"]
+    session["lname"] = request.form["lastname"]
+    movie = request.form["movie"]
+    ver = request.form["verdict"]
+
+    return redirect(
+        url_for("user", usr=user, mov=movie, rev=ver)
+    )
        
 @app.route("/<usr>/<mov>/<rev>")
 def user(usr,mov,rev):
     if "lname" in session:
      lname = session["lname"]
-     return render_template("user_review.html",movie=mov,name=usr,review=rev,lastname=lname)
+     return render_template("review_show.html",movie=mov,name=usr,review=rev,lastname=lname)
     else:
-        return render_template("reviews.html")
+        return render_template("newind.html")
 
 @app.route("/lname")
 def lname():
       if "lname" in session:
        lname = session["lname"]
-       return render_template("user_review.html",movie="mov",name="usr",review="rev",lastname=lname)
+       return render_template("show_review.html",movie="mov",name="usr",review="rev",lastname=lname)
       else:
-        return render_template("reviews.html")
+        return render_template("write_review.html")
 
 @app.route("/logout")
 def logout():
-    if "lname" in session:
-       lname = session["lname"]
-       flash(f"{lname} have successfully Logged Out!","info")
-       session.pop("lname",None)
-    return redirect(url_for("home"))
+       popping = session["firstname"]
+       flash(f"{popping} have successfully Logged Out!","info")
+       session.pop("firstname", None)
+       session.pop("lastname", None)
+       return redirect(url_for("home"))
 
 if __name__ == '__main__':
     app.run(debug=True)

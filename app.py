0eea03aed8b4a634c49fd959bb495f4566bbf09c
dhruvam Si <redfir347@gmail.com>
2026-06-17 18:34:04 +0530

    added database,and review showing page

--- app.py
+++ app.py
@@ -4,46 +4,61 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "diewreview"
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///users.sqlite3"  # fixed: 3 slashes
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///users.sqlite3"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 class users(db.Model):
     _id = db.Column("id", db.Integer, primary_key=True)
-    firstname = db.Column("firstname", db.String(30))
-    lastname = db.Column("lastname", db.String(30))
+    user_name = db.Column("user_name", db.String(30))
+    user_email = db.Column("user_email", db.String(30))
 
-    def __init__(self, firstname, lastname):
-        self.firstname = firstname
-        self.lastname = lastname
+    def __init__(self, user_name, user_email):
+        self.user_name = user_name
+        self.user_email = user_email
+
+class reviews(db.Model):
+    _id=db.Column("id",db.Integer,primary_key=True)
+    user_id=db.Column("user_id",db.Integer,db.ForeignKey("users._id"))
+    user_movie=db.Column("user_movie",db.String(50))
+    user_review=db.Column("user_review",db.String(2000))
+    
+    def __init__(self,user_id,user_movie,user_review,):
+        self.user_id=user_id
+        self.user_movie=user_movie
+        self.user_review=user_review
+        
 
 app.permanent_session_lifetime = timedelta(minutes=5)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
-        fname = request.form["faname"]
-        lname = request.form["laname"]
+        user_name = request.form["user_name"]
+        user_email = request.form["user_email"]
 
-        session["firstname"] = fname
-        session["lastname"] = lname
+        session["user_name"] = user_name
+        session["user_email"] = user_email
         session.permanent = True
 
         # save to DB if user doesn't already exist
-        existing = users.query.filter_by(firstname=fname, lastname=lname).first()
-        if not existing:
-            new_user = users(fname, lname)
+        exists = users.query.filter_by(user_name=user_name, user_email=user_email).first()
+        if not exists:
+            new_user = users(user_name, user_email)
             db.session.add(new_user)
             db.session.commit()
+            session["user_id"]=new_user._id
+        else:
+         session["user_id"]=exists._id
 
         return redirect(url_for("review"))
 
-    if "firstname" in session and "lastname" in session:
+    if "user_name" in session and "user_email" in session:
         flash("You are already logged in !", "info")
         return render_template(
             "newind.html",
-            faname=session["firstname"],
-            laname=session["lastname"]
+            user_name=session["user_name"],
+            user_email=session["user_email"]
         )
 
     return render_template("newind.html")
@@ -51,53 +66,55 @@ def home():
 
 @app.route("/review", methods=["GET", "POST"])
 def review():
+    # fix: bail out early if session data doesn't exist yet,
+    # instead of crashing with a KeyError
+    if "user_name" not in session or "user_email" not in session:
+        flash("Please enter your name and email first.", "info")
+        return redirect(url_for("home"))
+
     if request.method == "GET":
         return render_template(
             "write_review.html",
-            firstname=session["firstname"],
-            lastname=session["lastname"]
+            user_name=session["user_name"],
+            user_email=session["user_email"]
         )
 
-    user = request.form["firstname"]
-    session["lname"] = request.form["lastname"]
+    # POST: user submitted the review form
     movie = request.form["movie"]
     ver = request.form["verdict"]
 
-    return redirect(url_for("user", usr=user, mov=movie, rev=ver))
+    new_review = reviews(session["user_id"], movie, ver)
+    db.session.add(new_review)
+    db.session.commit()
+
+    flash(f"Review for '{movie}' saved!", "info")
+    return redirect(url_for("review"))
 
 
-@app.route("/<usr>/<mov>/<rev>")
-def user(usr, mov, rev):
-    if "firstname" not in session or "lastname" not in session:
-     return redirect(url_for("home"))
-    if "lname" in session:
-        lname = session["lname"]
-        return render_template("review_show.html", movie=mov, name=usr, review=rev, lastname=lname)
+
+@app.route("/user_reviews")
+def user_reviews():
+    if "user_id" not in session:
+        return redirect(url_for("home"))
     else:
-        return render_template("newind.html")
+        user_reviews_list=reviews.query.filter_by(user_id=session["user_id"]).all()
+        return render_template("user_reviews.html",values=user_reviews_list)
 
 
-@app.route("/lname")
-def lname():
-    if "lname" in session:
-        lname = session["lname"]
-        return render_template("show_review.html", movie="mov", name="usr", review="rev", lastname=lname)
-    else:
-        return render_template("write_review.html")
 
 
 @app.route("/logout")
 def logout():
-    if "firstname" in session:
-     popping = session["firstname"]
-     flash(f"{popping} have successfully Logged Out!", "info")
-     session.pop("firstname", None)
-     session.pop("lastname", None)
-     return redirect(url_for("home"))
+    if "user_name" in session:
+        popping = session["user_name"]
+        flash(f"{popping} have successfully Logged Out!", "info")
+        session.pop("user_name", None)
+        session.pop("user_email", None)
+    return redirect(url_for("home"))
 
 
 if __name__ == '__main__':
-    with app.app_context():  # fixed: no argument
+    with app.app_context():
         db.create_all()
     app.run(debug=True)
 
